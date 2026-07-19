7dccf76f7c0372aa4c79ec17e6122dd0832c955f
glacuevab <glacueva@uoc.edu>
2026-07-19 11:24:17 +0200

    fixed chart expense

--- database/migrations/2026_06_06_103731_recalculate_views.php
+++ database/migrations/2026_06_06_103731_recalculate_views.php
@@ -50,13 +50,18 @@ return new class extends Migration
                     mc.name,
                     YEAR(m.date) as year, 
                     MONTH(m.date) as month, 
-                    SUM(m.shared_amount) as total_amount
+                    SUM(CASE 
+                        WHEN mt.is_positive = 0 and is_compensation=0 THEN m.shared_amount
+                        WHEN (mt.is_positive = 1 and m.is_compensation = 1) THEN m.shared_amount
+                        ELSE -m.shared_amount
+                    END) as total_amount
                 FROM movements m
                 JOIN movement_categories mc ON m.movement_category_id = mc.id
                 JOIN movement_types mt ON mc.movement_type_id = mt.id
-                WHERE (mt.is_positive = 0 and m.is_compensation = false)
+                WHERE (mt.is_positive = 0)
                     OR (mt.is_positive = 1 and m.is_compensation = true)
-                GROUP BY m.user_id, m.account_id, mc.id, mc.name, YEAR(m.date), MONTH(m.date);
+                GROUP BY m.user_id, m.account_id, mc.id, mc.name, YEAR(m.date), MONTH(m.date)
+                HAVING total_amount > 0;
         ');
     }
 
